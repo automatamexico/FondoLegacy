40d73f550d3692a7027c494fd783df102c92faff
automatamexico <desarrollo.solucionesdelsu@gmail.com>
2026-02-05 21:05:58 -0600

    Create supabaseConfig.js

---
+++ src/lib/supabaseConfig.js
@@ -0,0 +1,4 @@
+export const SUPABASE_URL = "https://ubfkhtkmlvutwdivmoff.supabase.co";
+
+export const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InViZmtodGttbHZ1dHdkaXZtb2ZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4MTc5NTUsImV4cCI6MjA2NjM5Mzk1NX0.c0iRma-dnlL29OR3ffq34nmZuj_ViApBTMG-6PEX_B4";
