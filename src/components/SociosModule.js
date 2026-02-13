7a49186d623ddc740a444bf2b9c2c30816595eff
automatamexico <desarrollo.solucionesdelsu@gmail.com>
2026-02-12 18:27:58 -0600

    Update SociosModule.js

--- src/components/SociosModule.js
+++ src/components/SociosModule.js
@@ -897,7 +897,7 @@ if (ahorroRetiro) {
           </div>
         </div>
       )}
- {/* Modal Confirmar Registro */}
+{/* Modal Confirmar Registro */}
 {showConfirmRegistro && (
   <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
     <div className="bg-white rounded-2xl shadow-xl p-6 max-w-sm w-full text-center">
@@ -928,6 +928,7 @@ if (ahorroRetiro) {
     </div>
   </div>
 )}
+
       {/* Modal confirmar eliminación */}
       {showConfirmModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
